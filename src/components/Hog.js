0a14d5ef3e8d1321ef195bec0e883456faae61f3
rjwilson0531 <rjwilson0531@gmail.com>
2020-04-01 15:27:05 -0400

    index finished && git push

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -7,8 +7,18 @@ class Hog extends React.Component {
     render() {
         let {name, specialty, greased, weight} = this.props.hog
         console.log(this.props.hog.name)
+        let pic_name = name.split(' ').join('_').toLowerCase()
+        let picture = require(`../hog-imgs/${pic_name}.jpg`)
         return (
-            <h1> </h1>
+            <div className="pigTile">
+                <img src={picture}></img>
+                <h1> {name} </h1>
+                <p>{specialty}</p>
+                <p>{greased}</p>
+                <p>{weight}</p>
+
+            </div>
+            
         )
     }
 }
